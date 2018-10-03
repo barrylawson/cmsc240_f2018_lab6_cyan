a780e71474a897158d69013bade9ac41269cc597
cx9vc <cx9vc@mathcs02.richmond.edu>
2018-10-03 19:42:03 -0400

    MESSAGE INDICATING YOUR CHANGES

--- DoubleVector.cpp
+++ DoubleVector.cpp
@@ -5,42 +5,65 @@
 #include "DoubleVector.h"
 #include "CharacterVector.h"
 
-DoubleVector::DoubleVector()  {}
+DoubleVector::DoubleVector()  
+{
+   std::vector<double> doubleVector; 
+}
 DoubleVector::~DoubleVector() {}
 
 int DoubleVector::size() 
 {
-   return ...
+   return doubleVector.size();
 }
 
 // just return the double at the specified index; use the at() method rather
 // than [] so that an out_of_range error will be thrown for an invalid index
 double DoubleVector::get(int index) 
 { 
-   return ...
+   return doubleVector.at(index);
 }
 
 // if index is size-legitimate, put the value at that index;
 // otherwise, use push_back to append to the end of the vector
 void DoubleVector::put(double value, int index)
 {
+   int size = doubleVector.size();
+   if(index < size && index >=0)
+   {
+	doubleVector.at(index) = value;
+   }
+   else
+   {
+	doubleVector.push_back(value);
+   }
 }
 
 // use push_back to append
 void DoubleVector::put(double value)
 {
+   doubleVector.push_back(value);
+
 }
 
 // for each integer in integerVector, use static_cast<double> to append as a
 // double to doubleVector
 void DoubleVector::appendIntegerVector(IntegerVector& integerVector)
 {
+   for ( int i = 0; i < integerVector.size(); i++)
+   {
+	doubleVector.push_back(static_cast<double>(integerVector.get(i)));
+   }
+
 }
 
 // for each character in characteVector, use static_cast<double> to append as a
 // double to doubleVector
 void DoubleVector::appendCharacterVector(CharacterVector& characterVector)
 {
+   for ( int i = 0; i < characterVector.size(); i++)
+   {
+        doubleVector.push_back(static_cast<double>(characterVector.get(i)));
+   }
 }
 
 #endif
--- VectorTester.cpp
+++ VectorTester.cpp
@@ -34,7 +34,66 @@ int main()
    std::cout << "-------------" << std::endl;
    std::cout << "DoubleVector:" << std::endl;
    std::cout << "-------------" << std::endl;
+//   std::cout << "Testing default constructor" << std::endl;
+//   for ( int i = 0; i < dv.size(); i++)
+//   {
+//   	std::cout << dv.get(i);
+//   }
+//   std::cout <<"[]"<< std::endl; 
+//   std::cout << dv.size() << "[0]" << std::endl;
+//   std::cout << "-------------" << std::endl;
+   std::cout << "Testing put(value)" << std::endl;
+   dv.put(11.11);
+   for ( int i = 0; i < dv.size(); i++)
+   {
+        std::cout << dv.get(i)<<'\t';
+   }
+   std::cout <<"[11.11]"<< std::endl;
+   std::cout << dv.size() << "[1]" << std::endl;
+   dv.put(22.22);
+   for ( int i = 0; i < dv.size(); i++)
+   {
+        std::cout << dv.get(i)<<'\t';
+   }
+   std::cout <<"[11.11	22.22]"<< std::endl;
+   std::cout << dv.size() << "[2]" << std::endl;
 
+
+   std::cout << "-------------" << std::endl;
+   std::cout << "Testing put(value, index)" << std::endl;
+   dv.put(33.33, 5);
+   for ( int i = 0; i < dv.size(); i++)
+   {
+   	std::cout << dv.get(i) << '\t';
+   }
+   std::cout <<"[11.11	22.22	33.33]"<< std::endl; 
+   std::cout << dv.size() << "[3]" << std::endl;
+   
+   dv.put(44.44,0);
+   for ( int i = 0; i < dv.size(); i++)
+   {
+	std::cout << dv.get(i)<< '\t';
+   }
+   std::cout <<"[44.44	22.22	33.33]"<< std::endl;
+   std::cout << dv.size() << "[3]" << std::endl;
+   std::cout << "-------------" << std::endl;
+
+   std::cout << "Testing get" << std::endl;  
+   std::cout << dv.get(0) << "[44.44]" << std::endl;
+   std::cout << dv.get(1) << "[22.22]" << std::endl;
+   try
+    {
+	std::cout<< dv.get(10) << std::endl;         
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "exception caught: " << e.what() << '\n';
+    }
+   std::cout << "-------------" << std::endl;
+   
+   std::cout << "Testing size" << std::endl;
+   std::cout << dv.size() << "[3]" << std::endl;
+   
    //-------------------------------------------------------------------------
 
    // using empty IntegerVector, test appending cv & dv from above 
