cc0f86a74f8d83015d40bc728a213cbc16315dfe
cx9vc <cx9vc@mathcs02.richmond.edu>
2018-10-04 18:07:31 -0400

    Updating test

--- VectorTester.cpp
+++ VectorTester.cpp
@@ -116,44 +116,44 @@ int main()
 //   std::cout << dv.size() << "[0]" << std::endl;
 //   std::cout << "-------------" << std::endl;
    std::cout << "Testing put(value)" << std::endl;
-   dv.put(11.11);
+   dv.put(71.11);
    for ( int i = 0; i < dv.size(); i++)
    {
         std::cout << dv.get(i)<<'\t';
    }
-   std::cout <<"[11.11]"<< std::endl;
+   std::cout <<"[71.11]"<< std::endl;
    std::cout << dv.size() << "[1]" << std::endl;
-   dv.put(22.22);
+   dv.put(72.22);
    for ( int i = 0; i < dv.size(); i++)
    {
         std::cout << dv.get(i)<<'\t';
    }
-   std::cout <<"[11.11	22.22]"<< std::endl;
+   std::cout <<"[71.11	72.22]"<< std::endl;
    std::cout << dv.size() << "[2]" << std::endl;
 
 
    std::cout << "-------------" << std::endl;
    std::cout << "Testing put(value, index)" << std::endl;
-   dv.put(33.33, 5);
+   dv.put(73.33, 5);
    for ( int i = 0; i < dv.size(); i++)
    {
    	std::cout << dv.get(i) << '\t';
    }
-   std::cout <<"[11.11	22.22	33.33]"<< std::endl; 
+   std::cout <<"[71.11	72.22	73.33]"<< std::endl; 
    std::cout << dv.size() << "[3]" << std::endl;
    
-   dv.put(44.44,0);
+   dv.put(74.44,0);
    for ( int i = 0; i < dv.size(); i++)
    {
 	std::cout << dv.get(i)<< '\t';
    }
-   std::cout <<"[44.44	22.22	33.33]"<< std::endl;
+   std::cout <<"[74.44	72.22	73.33]"<< std::endl;
    std::cout << dv.size() << "[3]" << std::endl;
    std::cout << "-------------" << std::endl;
 
    std::cout << "Testing get" << std::endl;  
-   std::cout << dv.get(0) << "[44.44]" << std::endl;
-   std::cout << dv.get(1) << "[22.22]" << std::endl;
+   std::cout << dv.get(0) << "[74.44]" << std::endl;
+   std::cout << dv.get(1) << "[72.22]" << std::endl;
    try
     {
 	std::cout<< dv.get(10) << std::endl;         
@@ -190,13 +190,27 @@ int main()
    //-------------------------------------------------------------------------
 
    // using empty DoubleVector, test appending iv & cv from above
-//   DoubleVector dv2;
+
+   DoubleVector dv2;
 
    std::cout << std::endl;
    std::cout << "-------------------------" << std::endl;
    std::cout << "appended-to DoubleVector:" << std::endl;
    std::cout << "-------------------------" << std::endl;
-
+   
+   dv2.appendIntegerVector( iv );
+   for ( int i = 0; i < dv2.size(); i++)
+   {
+        std::cout << dv2.get(i)<< ',';
+   }
+   std::cout << "[65,70,81,89]"<<std::endl;
+  
+   dv2.appendCharacterVector( cv );
+   for ( int i = 0; i < dv2.size(); i++)
+   {
+        std::cout << dv2.get(i)<< ',';
+   }
+   std::cout << "[65,70,81,89,98(b),98(b)]"<<std::endl; 
    //-------------------------------------------------------------------------
 
    return 0;
